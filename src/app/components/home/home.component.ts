40d46ad7bd902c38783bb294067df92a43b34220
Luis Angel <soyluisbits@gmail.com>
2019-02-05 11:44:42 -0500

    Agrega servicio de usuarios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/interfaces/user';
+
 
 @Component({
   selector: 'app-home',
@@ -7,47 +9,9 @@ import { User } from 'src/app/interfaces/user';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    public friends: User[];
-    constructor() {
-        let usuario1: User = {
-            nick: 'Eduardo',
-            age: 24,
-            email: 'ed@aoe.aoe',
-            friend: true,
-            uid: 1
-        };
-        let usuario2: User = {
-            nick: 'Freddy',
-            age: 28,
-            email: 'fred@aoe.aoe',
-            friend: true,
-            uid: 2
-        };
-        let usuario3: User = {
-            nick: 'Yuliana',
-            age: 18,
-            email: 'yuli@aoe.aoe',
-            friend: true,
-            uid: 3
-        };
-        let usuario4: User = {
-            nick: 'Ricardo',
-            age: 17,
-            email: 'rick@aoe.aoe',
-            friend: false,
-            uid: 4
-        };
-        let usuario5: User = {
-            nick: 'Marcos',
-            age: 30,
-            email: 'marcos@aoe.aoe',
-            friend: false,
-            uid: 5
-        };
-
-        this.friends = [
-            usuario1, usuario2, usuario3, usuario4, usuario5
-        ];
+    friends: User[];
+    constructor(private service: UserService) {
+        this.friends = this.service.getFriends();
     }
 
   ngOnInit() {
