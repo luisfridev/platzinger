5f3f56dce869a1380db5f94cbcf5b80df035f82a
Luis Angel <soyluisbits@gmail.com>
2019-02-05 15:50:04 -0500

    Agregando más estilos para login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  operation: string;
 
-  constructor() { }
+  constructor() {
+      this.operation = 'login';
+  }
 
   ngOnInit() {
   }
